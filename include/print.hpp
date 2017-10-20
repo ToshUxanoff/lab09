5410d8d2d7e64211f767debbe658ae194d291dba
ToshUxanoff <tosha.uhanoff@yandex.ru>
2017-10-20 18:18:40 +0300

    updated doxygen

--- include/print.hpp
+++ include/print.hpp
@@ -1,5 +1,15 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+/*!
+Печатает строку text в стандартный поток
+\param text, out - строка для вывода, стандартный поток 
+\return Ничего не возвращает
+*/
 void print(const std::string& text, std::ostream& out = std::cout);
-void print(const std::string& tect, std::ofstream& out);
+/*!
+Печатает строку text в поток out
+\param text, out - строка для вывода, поток для вывода
+\return Ничего не возвращает
+*/
+void print(const std::string& text, std::ofstream& out);
